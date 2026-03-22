144cb66678d689c3c025dead91606d013642d176
Oliwier Juncewicz <122737448+JuncProgramming@users.noreply.github.com>
2026-03-22 10:48:47 +0100

    Set up tailwind and eslint + prettier

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules']),
@@ -11,15 +11,18 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      eslintPluginPrettier,
+      eslintPluginPrettier
     ],
     languageOptions: {
       ecmaVersion: 2022,
-      globals: globals.node,
+      globals: globals.node
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' }
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn'
     }
-  },
-]);
+  }
+])
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './styles.css'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <h1>Test</h1>
+    </QueryClientProvider>
+  </StrictMode>
 )
